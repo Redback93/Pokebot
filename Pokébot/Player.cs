3005c40f1213d71049da2d4bc447bbfb08e9176c
Redback93 <redback93@hotmail.com>
2015-01-07 14:46:00 +0800

    Initial Commit

---
+++ Pokébot/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Pokébot
+{
+    class Player
+    {
+        public string Username;
+        public int Points;
+        public int Level;
+        public int Pokemon;
+        [JsonIgnore]
+        public bool Tracking;
+
+        public Player(string username)
+        {
+            this.Username = username;
+            this.Points = 0;
+            this.Level = 0;
+            this.Pokemon = 0;
+            this.Tracking = true;
+        }
+    }
+}
---
+++ Pokébot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pokébot
+{
+	//Wrapper class for the IRC bot
+	//Reads configuration and starts bot
+	class Launcher
+	{
+		//Entry point for all C# code
+		public static void Main(string[] args)
+		{
+			//Load the IRC settings
+			string username = "";
+            string password = "";
+			string channel = "";
+
+
+			//Create a new instance of the IRC bot
+			IRCBot newBot = new IRCBot(username, password, channel);
+			newBot.Run();
+		}
+	}
+}
