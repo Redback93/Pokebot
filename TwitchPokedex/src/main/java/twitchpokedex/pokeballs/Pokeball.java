f0df680139151b90f90d293a937009414d19b754
Utilitybread <potykryan@gmail.com>
2015-01-10 01:44:02 -0600

    ACTUALLY added the new pokeballs. (For real this time).

--- TwitchPokedex/src/main/java/twitchpokedex/pokeballs/Pokeball.java
+++ TwitchPokedex/src/main/java/twitchpokedex/pokeballs/Pokeball.java
@@ -8,9 +8,24 @@ import lombok.AccessLevel;
 @AllArgsConstructor
 public enum Pokeball
 {
-	UltraBall(1),
-	SafariBall(2),
-	FireBall(3);
+	Normalball(1), //Normal
+	Dojoball(2), //Fighting
+	LavaBall(3), //Fire
+	Wingball(4), //Flying
+	Leafball(5), //Grass
+	Toxicball(6), //Poison
+	Earthball(7), //Ground
+	Stoneball(8), //Rock
+	Netball(9), //Bug
+	Phantomball(10), //Ghost
+	Ironball(11), //Steel
+	Diveball(12), //Water
+	Thunderball(13), //Electric
+	Mindball(14), //Psychic
+	Frostball(15), //Ice
+	Shadowball(16), //Dark
+	Dragonball(17), //Dragon
+	Fairyball(18); //Fairy
 	
 	private final int id;
 }
